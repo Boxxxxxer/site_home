a8fe28d3b0f7f5de7c66af57eeb601b335f4375e
user <user@kali.kali>
2024-07-30 11:24:08 +0300

    Initial commit

---
+++ my_app/management/commands/clear_cache.py
@@ -0,0 +1,10 @@
+# my_app/management/commands/clear_cache.py
+from django.core.management.base import BaseCommand
+from django.core.cache import cache
+
+class Command(BaseCommand):
+    help = 'Clears the cache'
+
+    def handle(self, *args, **kwargs):
+        cache.clear()
+        self.stdout.write(self.style.SUCCESS('Cache cleared'))
